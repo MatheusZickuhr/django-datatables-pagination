21a8bcda00ee5b923854f116b2a344075959fd5a
Matheus Zickuhr <matheus@singular.inf.br>
2020-08-14 11:54:13 -0300

    Update version. Fixed template tracking (pypi)

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
+    packages=find_packages(),
+    include_package_data=True,
     name='django-datatables-pagination',
-    packages=['django_datatables_pagination'],
-    version='0.1.2',
+    version='0.1.5',
     license='MIT',
     description='A Django ListView integration with datatables library.',
     author='Matheus Zickuhr',
